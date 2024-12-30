1d4118153e73f2958238e4883ff7845ee6bcddc3
e <jannatul0040@gmail.com>
2024-12-30 17:25:15 +0600

    Hash password before saving user and clear password after save

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,11 +1,12 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config({
-  path: path.join(process.cwd(), ".env"),
+  path: path.join(process.cwd(), '.env'),
 });
 
 export default {
   port: process.env.PORT,
   db_url: process.env.DB_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
 };
--- src/module/user/user.model.ts
+++ src/module/user/user.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from 'mongoose';
 import { IUser } from './user.interface';
+import bcrypt from 'bcrypt';
+import config from '../../app/config';
 
 const userSchema = new Schema<IUser>(
   {
@@ -30,6 +32,21 @@ const userSchema = new Schema<IUser>(
     timestamps: true,
   },
 );
+
+userSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
 
 const User = model<IUser>('User', userSchema);
 export default User;
